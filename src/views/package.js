bffb8d040be091f9bd9e673d313331110a44308b
tomaszpankowski <pankowski.email@gmail.com>
2022-01-20 19:40:44 +0100

    modified routing

---
+++ src/views/package.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Package extends Component{
+    render(){
+        return(        
+            <Container fluid className="package-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Package;
