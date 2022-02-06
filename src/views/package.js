f9165f1f752d1f62bded3190ec23edba7a41a304
tomaszpankowski <pankowski.email@gmail.com>
2022-02-06 22:03:36 +0100

    layout done

--- src/views/package.js
+++ src/views/package.js
@@ -1,11 +1,19 @@
 import React,{Component} from "react";
 import Container from "../../node_modules/react-bootstrap/Container";
+import PackageS1 from "../components/package-s1";
+import PackageS2 from "../components/package-s2";
+import PackageS3 from "../components/package-s3";
+import PackageS4 from "../components/package-s4";
+
 
 class Package extends Component{
     render(){
         return(        
-            <Container fluid className="package-s1 d-flex align-items-center minh-100vh">
-                
+            <Container fluid className="minh-footer-adj p-0">
+                <PackageS1/>
+                <PackageS2/>
+                <PackageS3/>
+                <PackageS4/>
             </Container>    
         );
     }
